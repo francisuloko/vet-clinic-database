1fd99cfebceff66724294836bbf29d8643065652
Mihndim Julius <mihndim2016@gmail.com>
2021-09-28 12:54:27 -0400

    Add column to owners table and Insert data into owners and visits table

--- schema.sql
+++ schema.sql
@@ -48,3 +48,7 @@ CREATE TABLE visits(
 
 INSERT INTO animals (name) VALUES ('Agumon'), ('Gabumon'), ('Pikachu'), ('Devimon'), ('Charmander'), ('Plantmon'), ('Squirtle'), ('Angemon'), ('Boarmon'), ('Blossom');
 ​INSERT INTO vets (name) VALUES ('William Tatcher'), ('Maisy Smith'), ('Stephanie Mendez'), ('Jack Harkness');
+
+ALTER TABLE owners ADD COLUMN email VARCHAR(120);
+INSERT INTO visits (animal_id, vet_id, date_of_visit) SELECT * FROM (SELECT id FROM animals) animal_ids, (SELECT id FROM vets) vets_ids, generate_series('1980-01-01'::timestamp, '2021-01-01', '4 hours') visit_timestamp;
+insert into owners (full_name, email) select 'Owner ' || generate_series(1,2500000), 'owner_' || generate_series(1,2500000) || '@mail.com';
